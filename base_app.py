90e48ade2b638959b001a9fca00f637ea2e94ee1
Ntandoyenkosi Biyela <biyelantandoyenkosi12@gmail.com>
2024-07-26 11:36:28 +0200

    Updated base file

--- base_app.py
+++ base_app.py
@@ -29,15 +29,47 @@ def recommend_movies(user_id, ratings, movies, num_recommendations=3):
     
     return recommendations
 
-# Streamlit app
-st.title("Simple Movie Recommender System")
+# Streamlit app with pages
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "About Us", "Recommendation"])
 
-# User input for UserID
-user_id = st.number_input("Enter User ID:", min_value=1, max_value=5, value=1)
+if page == "Home":
+    st.title("Welcome to the Movie Recommender System")
+    st.write("""
+    This application helps you find movies that you'll love based on your preferences and ratings.
+    
+    Navigate to the Recommendation page to get personalized movie recommendations.
+    """)
 
-if st.button("Recommend Movies"):
-    recommendations = recommend_movies(user_id, ratings, movies)
-    st.write("Recommended Movies for User ID {}:".format(user_id))
-    st.table(recommendations[['Title', 'Genre']])
+elif page == "About Us":
+    st.title("About Us")
+    st.write("""
+    Welcome to the Simple Movie Recommender System!
 
-# Run the Streamlit app: streamlit run app.py
+    Our mission is to help you find movies that you'll love based on your preferences and ratings.
+    
+    This system uses collaborative filtering to suggest movies you haven't seen yet but might enjoy based on the ratings of other users with similar tastes.
+
+    **Team Members:**
+    - Neo Modibedi: Team Leader
+    - Melody Msimango: Project Manager
+    - Priscilla Matuludi: GitHub Manager
+    - Keryn Rabe: Organizer
+    - Tumelo Matamane
+    - Ntandoyenkosi Biyela
+
+    Enjoy your movie recommendations!
+    """)
+
+elif page == "Recommendation":
+    st.title("Movie Recommendations")
+    
+    # User input for UserID
+    user_id = st.number_input("Enter User ID:", min_value=1, max_value=5, value=1)
+
+    if st.button("Recommend Movies"):
+        recommendations = recommend_movies(user_id, ratings, movies)
+        st.write("Recommended Movies for User ID {}:".format(user_id))
+        st.table(recommendations[['Title', 'Genre']])
+
+
