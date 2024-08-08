1fb7ef799ee905a86a967ebee8fab35bcb7ba545
Keryn Rabe <145427843+Kezbee@users.noreply.github.com>
2024-08-08 17:23:57 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+pip install scikit-learn
 import sklearn
 from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
